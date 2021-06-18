1d0cb70b772876bac05bd1156eecf2e824db9f84
belaid reda <abdelkadirbelaid@gmail.com>
2021-06-18 15:41:26 +0100

    Develeopement de la page login

--- gestionDeStock/src/app/app.module.ts
+++ gestionDeStock/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageLoginComponent
   ],
   imports: [
     BrowserModule
