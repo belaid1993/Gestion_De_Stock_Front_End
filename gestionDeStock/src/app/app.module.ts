a7a3e81a8d4e05f487edf9e35daf80f4dc6c96b8
belaid reda <abdelkadirbelaid@gmail.com>
2021-06-18 22:03:41 +0100

    Develeopement de la page d'inscription

--- gestionDeStock/src/app/app.module.ts
+++ gestionDeStock/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageLoginComponent
+    PageLoginComponent,
+    PageInscriptionComponent,
+    PageDashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
